bc79258daf9434a8d8f1bb6b26819f01a11d9631
Stelusno <stelusno@gmail.com>
2023-08-04 18:53:37 -0400

    Hardcoded data to check Type Script

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data'
 
 export default function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,17 @@ export default function App() {
   return (
     <div>
       <header>The Spot</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+              <li key={product.slug}>
+                <img className='product-image' src={product.image} alt={product.name} />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </li>
+            ))}
+        </ul>
+      </main>
       <footer>All rights reserved</footer>
     </div>
   )
---
+++ client/src/data.ts
@@ -0,0 +1,59 @@
+import { Product } from './types/Products';
+
+export const sampleProducts: Product[] = [
+    {
+        name: 'Nike Slim shirt', 
+        slug: 'nike-slim-shirt',
+        category: 'Shirts', 
+        image: '../images/p1.jpg', 
+        price: 30,
+        countInStock: 4,
+        brand: 'Nike', 
+        rating: 4.5,
+        description: 'cotton high quality shirt'
+    }, 
+    {
+        name: 'Calvin Klein shirt', 
+        slug: 'calvin-klein-shirt',
+        category: 'Shirts', 
+        image: '../images/p2.jpg', 
+        price: 40,
+        countInStock: 3,
+        brand: 'Calvin Klein', 
+        rating: 5,
+        description: 'modal high quality shirt'
+    }, 
+    {
+        name: 'Adidas Fitted Shirt', 
+        slug: 'adidas-fitted-shirt',
+        category: 'Shirts', 
+        image: '../images/p3.jpg', 
+        price: 25,
+        countInStock: 6,
+        brand: 'Adidas', 
+        rating: 3,
+        description: 'cotton shirt'
+    },
+    {
+        name: 'Adidas League Pants', 
+        slug: 'adidas-league-pants',
+        category: 'pants', 
+        image: '../images/p4.jpg', 
+        price: 45,
+        countInStock: 20,
+        brand: 'Adidas', 
+        rating: 5,
+        description: 'fitted quality track pants'
+    },
+    {
+        name: 'Ralph Lauren Jeans', 
+        slug: 'ra;ph-lauren-jeans',
+        category: 'pants', 
+        image: '../images/p5.jpg', 
+        price: 60,
+        countInStock: 4,
+        brand: 'Ralph Lauren', 
+        rating: 4.5,
+        description: 'high quality denim jeans'
+    }
+]
